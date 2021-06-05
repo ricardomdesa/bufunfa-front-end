77c2eff434ac0eb637824379c40594d481229392
Ricardo Sa <ricardo.mdesa@gmail.com>
2021-06-05 18:52:43 -0300

    updates form and refactor

--- src/components/forms/MovimentForm.vue
+++ src/components/forms/MovimentForm.vue
@@ -1,133 +1,143 @@
 <template>
-  <form class="form" name="movimentForm" @submit.prevent="insert">
-    <div class="field is-horizontal">
-      <div class="field-label is-normal">
-        <label class="label">Corretora</label>
+  <form class="box" name="movimentForm" @submit.prevent="insert">
+    <div class="field">
+      <label class="label">Corretora</label>
+      <div class="control">
+        <input
+          class="input"
+          type="text"
+          placeholder="XP, Inter"
+          v-model="corretora"
+          required
+        />
       </div>
-      <div class="field-body">
-        <div class="field">
-          <p class="control">
-            <input
-              class="input"
-              type="text"
-              placeholder="corretora..."
-              v-model="corretora"
-            />
-          </p>
+    </div>
+    <div class="field">
+      <label class="label">Ação</label>
+      <div class="control">
+        <span class="select is-fullwidth">
+          <select v-model="selectedStock">
+            <option
+              v-for="stock in getStockList"
+              :key="stock.stock_code"
+              :value="stock.stock_code"
+              required
+              >{{ stock.stock_code }}</option
+            >
+          </select>
+        </span>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Quantidade</label>
+      <div class="control">
+        <input
+          class="input"
+          type="number"
+          placeholder="0"
+          v-model="quantity"
+          required
+        />
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Preço</label>
+      <div class="control">
+        <input
+          class="input"
+          type="number"
+          placeholder="0"
+          v-model="price"
+          required
+        />
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Data operacao</label>
+      <div class="control">
+        <input
+          class="input"
+          type="date"
+          placeholder="YYYY-mm-dd"
+          v-model="opDate"
+        />
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Tipo operacao</label>
+      <div class="control">
+        <div class="select block is-fullwidth">
+          <select v-model="selectedType">
+            <option v-for="type in typeList" :key="type" :value="type">{{
+              type
+            }}</option>
+          </select>
         </div>
-      </div>
-    </div>
-    <div class="field is-horizontal ">
-      <div class="field-label is-normal">
-        <label class="label">Ação</label>
-      </div>
-      <select class="select is-fullwidth" v-model="selectedStock">
-        <option
-          v-for="stock in getStockList"
-          :key="stock.stock_code"
-          :value="stock.stock_code"
-          placeholder="Selecione ação"
-          >{{ stock.stock_code }}</option
-        >
-      </select>
-    </div>
-    <div class="field is-horizontal">
-      <div class="field-label is-normal">
-        <label class="label">Quantidade</label>
-      </div>
-      <div class="field-body">
-        <div class="field">
-          <p class="control">
-            <input
-              class="input"
-              type="number"
-              placeholder="qtde"
-              v-model="quantity"
-            />
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="field is-horizontal">
-      <div class="field-label is-normal">
-        <label class="label">Preço</label>
-      </div>
-      <div class="field-body">
-        <div class="field">
-          <p class="control">
-            <input
-              class="input"
-              type="number"
-              placeholder="preço"
-              v-model="price"
-            />
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="field is-horizontal">
-      <div class="field-label is-normal">
-        <label class="label">Data operacao</label>
-      </div>
-      <!-- <DatePicker :v-model="dateRange" :dateLimitInDays="7" /> -->
-    </div>
-    <div class="field is-horizontal">
-      <div class="field-label is-normal">
-        <label class="label">Tipo operacao</label>
-      </div>
-      <div class="select block is-fullwidth">
-        <select v-model="selected">
-          <option
-            v-for="type in typeList"
-            :key="type"
-            :value="type"
-            placeholder="Selecione o tipo"
-            >{{ type }}</option
-          >
-        </select>
       </div>
     </div>
     <hr />
-    <div class="block">
-      <button class="button is-info submit">
+    <div class="block level">
+      <button class="button is-info submit level-item">
         Adicionar
       </button>
+      <button class="button is-danger level-item" @click="cancelBtn">Cancelar</button>
     </div>
   </form>
 </template>
 <script>
 import { mapActions, mapGetters } from "vuex";
-// import DatePicker from "@/components/shared/input/DatePicker.vue";
 export default {
   name: "moviment-form",
-  components: {
-    // DatePicker,
-  },
   computed: {
     ...mapGetters(["getStockList"]),
   },
+  data() {
+    return {
+      selectedType: null,
+      selectedStock: null,
+      corretora: "",
+      quantity: null,
+      price: null,
+      opDate: null,
+      typeList: ["Compra", "Venda"],
+    };
+  },
   methods: {
-    ...mapActions(["GET_STOCKS"]),
+    ...mapActions(["GET_STOCKS", "INSERT_TRANSACTION"]),
     insert() {
-      console.log("insert form submmit");
+      const {
+        selectedType,
+        selectedStock,
+        corretora,
+        quantity,
+        price,
+        opDate,
+      } = this;
+
+      if (selectedStock && selectedType) {
+        this.INSERT_TRANSACTION({
+          selectedType,
+          selectedStock,
+          corretora,
+          quantity,
+          price,
+          opDate,
+        });
+
+        this.$emit("close");
+      }
+    },
+    cancelBtn() {
       this.$emit("close");
     },
   },
   mounted() {
     this.GET_STOCKS();
   },
-  data() {
-    return {
-      selected: "",
-      selectedStock: "Selecione uma ação",
-      corretora: "",
-      quantity: 100,
-      price: 0.0,
-      opType: null,
-      data: "",
-      typeList: ["Compra", "Venda"],
-      dateRange: null,
-    };
-  },
 };
 </script>
+<style scoped>
+label {
+  text-align: left;
+}
+</style>
