d7195ba064918eba9af32625eeb7f08b7e43ad9c
Ricardo Sa <ricardo.mdesa@gmail.com>
2021-06-05 11:08:21 -0300

    refactors form structure

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -4,7 +4,7 @@
   </div>
 </template>
 <script>
-import SignUpForm from "@/components/SignUpForm.vue";
+import SignUpForm from "@/components/forms/SignUpForm.vue";
 export default {
   name: "App",
   components: {
